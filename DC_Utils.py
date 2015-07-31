64588d1daa53a914c275b9bc4d9a38a9419b2bde
dubeg <guillaume.dube.girard@gmail.com>
2015-07-31 16:56:14 -0400

    Restructure dcutils

--- DC_Utils.py
+++ DC_Utils.py
@@ -2,8 +2,22 @@ import sublime
 import sublime_plugin
 import os
 
+# -------------------------------------------------------------------
+# Metadata about a widget theme
+# -------------------------------------------------------------------
+class WidgetType:
+    def __init__(self, typeName, keyPrefix, filePrefix, fileExt):
+        self.Name = typeName
+        self.SettingKey = keyPrefix + typeName.lower()
+        self.SettingFilename = filePrefix + typeName + fileExt
 
+# ------------------------------------------------------------------- 
+# Utils: contains various methods manage widget themes.
+# -------------------------------------------------------------------
 class DCUtils:
+    # Private
+    __widgetTypes = None
+    # Public
     USER_PREFERENCES = 'Preferences.sublime-settings'
     USER_WIDGET_SETTINGS = 'Widget - DC_3.sublime-settings'
     COLOR_SCHEME_KEY = 'color_scheme'
@@ -20,25 +34,9 @@ class DCUtils:
         "DarkLight",
         "GrayDark"
         ]
+
     @staticmethod
-    def getWidgetExt():
-        if IS_DEV:
-            return WIDGET_FILE_EXT_DEV
-        else:
-            return WIDGET_FILE_EXT_PROD
-
-
-class WidgetType(object):
-    def __init__(self, typeName, keyPrefix, filePrefix, fileExt):
-        self.Name = typeName
-        self.SettingKey = keyPrefix + typeName.lower()
-        self.SettingFilename = filePrefix + typeName + fileExt
-
-
-class SetWidget(sublime_plugin.WindowCommand):
-    WidgetTypes = None
-
-    def __initTypes(self):
+    def __initTypes():
         types = dict()
         for i,typeName in enumerate(DCUtils.WIDGET_TYPENAMES):
             types[typeName] = WidgetType(
@@ -49,75 +47,123 @@ class SetWidget(sublime_plugin.WindowCommand):
                 )
         return types
 
-    def run(self, widgetName):
-        if SetWidget.WidgetTypes == None:
-            SetWidget.WidgetTypes = self.__initTypes()
+    @staticmethod
+    def getWidgetExt():
+        if DCUtils.IS_DEV:
+            return DCUtils.WIDGET_FILE_EXT_DEV
+        else:
+            return DCUtils.WIDGET_FILE_EXT_PROD
 
-        # Retrieve widgetType
-        widget = SetWidget.WidgetTypes.get(widgetName)
+    @staticmethod
+    def getWidgetTypes():
+        if DCUtils.__widgetTypes == None:
+            DCUtils.__widgetTypes = DCUtils.__initTypes()        
+        return DCUtils.__widgetTypes;
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Set Widget Theme
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~    
+    def setWidgetTheme(widgetName):
+        widgetTypes = DCUtils.getWidgetTypes()
+
+        # Retrieve WidgetType
+        widget = widgetTypes.get(widgetName)
         if widget == None:
             print('[SetDCWidget] Unknown widgetName: ' + widgetName) 
             return
 
         # Load preferences
-        pref = sublime.load_settings(USER_PREFERENCES)
+        pref = sublime.load_settings(DCUtils.USER_PREFERENCES)
         
         # Remove existing widget keys
-        for key, value in SetWidget.WidgetTypes.items():
+        for key, value in widgetTypes.items():
             pref.erase(value.SettingKey)
         
         # Set Widget key
         pref.set(widget.SettingKey, True)
 
         # Change setting in Widget - DC.sublime-settings
-        widgetSettings = sublime.load_settings(USER_WIDGET_SETTINGS)
-        widgetSettings.set(COLOR_SCHEME_KEY, COLOR_SCHEME_VALUE.format(widget.SettingFilename))
+        widgetSettings = sublime.load_settings(DCUtils.USER_WIDGET_SETTINGS)
+        widgetSettings.set(
+            DCUtils.COLOR_SCHEME_KEY, 
+            DCUtils.COLOR_SCHEME_VALUE.format(widget.SettingFilename)
+            )
+
+        return None
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Save Widget Settings
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+    def saveWidgetSettings():
+        sublime.save_settings(DCUtils.USER_PREFERENCES)
+        sublime.save_settings(DCUtils.USER_WIDGET_SETTINGS)        
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Toggle Widget Development env. & settings
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+    def toggleWidgetDev():
+        hintON = "ON"
+        hintOFF = "OFF"
+        pluginDir = os.path.dirname(os.path.realpath(__file__))
+        renameCount = 0
+
+        # Get infos related to current status
+        currentExt = DCUtils.getWidgetExt()
+        # Toggle
+        DCUtils.IS_DEV = not DCUtils.IS_DEV
+        # Get infos related to new status
+        newExt = DCUtils.getWidgetExt()
+        statusHint = hintON if DCUtils.IS_DEV else hintOFF
+        # Rename widget files if necessary
+        for filename in os.listdir( pluginDir ):
+            (name, ext) = os.path.splitext(filename)  
+
+            if ext == currentExt:  
+                newFilename = name + newExt
+                os.rename( 
+                    os.path.join(pluginDir, filename), 
+                    os.path.join(pluginDir, newFilename)
+                    )    
+                renameCount += 1 
+        
+        # TODO: write to Widget - DC3.sublime-settings
+        # - User
+        # - Package
+
+        # Display cmd status
+        sublime.status_message("DEV is " + statusHint + ", Renamed: " + str(renameCount))
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Is this Plugin Packaged/zipped
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~        
+    def isPluginPackaged():
+        packageFileExt = ".sublime-package"
+        isPackaged = (packageFileExt in __file__)
+        return isPackaged
+
+# ===================================================================
+# Section: Plugin Window Commands
+# ===================================================================
+class SetWidget(sublime_plugin.WindowCommand):
+    def run(self, widgetName):
+        DCUtils.setWidgetTheme(widgetName)
+        return None
 
 
 class SaveWidgetSettings(sublime_plugin.WindowCommand):
     def run(self):
-        sublime.save_settings(DCUtils.USER_PREFERENCES)
-        sublime.save_settings(DCUtils.USER_WIDGET_SETTINGS)
+        DCUtils.saveWidgetSettings()
+        return None
 
 
 class ToggleWidgetDev(sublime_plugin.WindowCommand):
     def run(self):
-        hintON = "ON"
-        hintOFF = "OFF"
-
-        # Toggle
-        DCUtils.IS_DEV = not DCUtils.IS_DEV
-        fileExt = DCUtils.getWidgetExt()
-        currentHint = hintON if DCUtils.IS_DEV else hintOFF
-            
-        #for filename in os.listdir("."):
-        #     if filename.startswith("cheese_"):    
-        #         os.rename(filename, filename[7:])     
-
-        sublime.status_message("DEV is " + currentHint)
-
-    def is_enabled(self):
-        packageFileExt = ".sublime-package"
-
-        # Do not run if plugin is packaged/zipped.
-        ok_to_run = False
-        if packageFileExt not in __file__:
-            ok_to_run = True
-
-        return ok_to_run
-        # Returns true if the command is able to be run at this time. 
-        # The default implementation simply always returns True.
-
-    def is_visible(self):
-        return True
-        # Returns true if the command should be shown in the menu at this time. 
-        # The default implementation always returns True.
-
-    def description(self):
-        # hintON = "[ON]"
-        # hintOFF = "[OFF]"
-        # currentHint = hintON if IS_DEV else hintOFF 
+        DCUtils.toggleWidgetDev()
         return None
-        # Returns a description of the command with the given arguments. 
-        # Used in the menu, if no caption is provided. 
-        # Return None to get the default description.
+
+    # Is Enabled:
+    # Returns true if the command is able to be run at this time. 
+    # The default implementation simply always returns True.
+    def is_enabled(self):
+        canBeRun = not DCUtils.isPluginPackaged()
+        return canBeRun
